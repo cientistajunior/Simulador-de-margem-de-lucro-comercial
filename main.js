bb6501f26fc591ff6daf0dde6cb7a8a09661d5cd
Wilson Neves Moreira Junior <wilson.moreira@tse.jus.br>
2026-07-06 19:05:17 -0300

    update incluindo constantes no main.js para poder conversar com o codigo

---
+++ main.js
@@ -0,0 +1,5 @@
+const form = document.querySelector("form");
+const costPrice = document.querySelector("#costPrice");
+const costSale = document.querySelector("#costSale");
+const result = document.querySelector(".result");
+
